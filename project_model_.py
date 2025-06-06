bac84f5b18fcc597cefb34eb9cfe3bbdf8e84702
Abdul2734 <abdulsrh6488@gmail.com>
2025-06-06 07:03:20 +0530

    Update project_model_.py

--- project_model_.py
+++ project_model_.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 import joblib
 
